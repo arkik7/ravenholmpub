e5bf498f1a011229ba590f00f33e6365a0429740
achiko06 <achiko06@gmail.com>
2023-02-16 12:25:39 +0100

    dailymenu style

--- pages/daily-menu.tsx
+++ pages/daily-menu.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import React from 'react'
+import styles from '@/styles/DailyMenu.module.css'
 
 const DailyMenu = () => {
   return (
@@ -8,7 +9,48 @@ const DailyMenu = () => {
         <title>Daily Menu|RavenHolmPub</title>
         <meta name="description" content="Restaurant website demo, Daily Menu page" />
       </Head>
-      <div>Daily Menu</div>
+      <div className={styles.notepadbody}>
+        <div className={styles.notepad}>
+          <div className={styles.top}></div>
+          <div className={styles.paper}>
+            <h2>Monday</h2>
+            <ul>
+              <li>Item sadsadsa dasdsadasd dasdsadsad</li>
+              <li>Item sadsadsa dasdsadasd dasdsadsad</li>
+              <li>Item sadsadsa dasdsadasd dasdsadsad</li>
+            </ul>
+
+            <h2>Monday</h2>
+            <ul>
+              <li>Item sadsadsa dasdsadasd dasdsadsad</li>
+              <li>Item sadsadsa dasdsadasd dasdsadsad</li>
+              <li>Item sadsadsa dasdsadasd dasdsadsad</li>
+            </ul>
+
+            <h2>Monday</h2>
+            <ul>
+              <li>Item sadsadsa dasdsadasd dasdsadsad</li>
+              <li>Item sadsadsa dasdsadasd dasdsadsad</li>
+              <li>Item sadsadsa dasdsadasd dasdsadsad</li>
+            </ul>
+
+            <h2>Monday</h2>
+            <ul>
+              <li>Item sadsadsa dasdsadasd dasdsadsad</li>
+              <li>Item sadsadsa dasdsadasd dasdsadsad</li>
+              <li>Item sadsadsa dasdsadasd dasdsadsad</li>
+            </ul>
+
+            <h2>Monday</h2>
+            <ul>
+              <li>Item sadsadsa dasdsadasd dasdsadsad</li>
+              <li>Item sadsadsa dasdsadasd dasdsadsad</li>
+              <li>Item sadsadsa dasdsadasd dasdsadsad</li>
+            </ul>
+       
+          </div>
+        </div>
+      </div>
     </>
   )
 }
